9ec00f6e127996219a676bc705ad231483a554e1
Aaftab K. Ansari <68555868+aaftab-ansari-15@users.noreply.github.com>
2024-09-24 19:25:15 +0530

    todays last commit

---
+++ src/components/SidebarLeft.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SidebarLeft = () => {
+  return <div>Sidebar</div>;
+};
+
+export default SidebarLeft;
---
+++ zemplemented.md
@@ -0,0 +1,1 @@
+navbar: ref for mouceover, pills style button and div,
